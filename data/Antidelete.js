984e7c8f3af0c888aa05c61f9d43e3afc782941b
aloneboyyss1234-bit <aloneboyyss1234@gmail.com>
2026-09-23 00:52:06 +0500

    Update Antidelete.js

--- data/Antidelete.js
+++ data/Antidelete.js
@@ -22,4 +22,4 @@ const setAntideleteStatus = async (chatId, status) => {
 };
 
 module.exports = { Antidelete, getAntideleteStatus, setAntideleteStatus };
-// ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴀʀꜱʟᴀɴ-ᴍᴅ
+// ᴘᴏᴡᴇʀᴇᴅ ʙʏ zeeshu_botz
